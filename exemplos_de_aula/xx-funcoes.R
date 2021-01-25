399bddad99addc44f9cac09c64beaf0f081887e7
Beatriz Milz <beatriz.milz@hotmail.com>
2021-01-25 15:42:52 -0300

    materiais para aula 4 - funções

--- exemplos_de_aula/xx-funcoes.R
+++ exemplos_de_aula/xx-funcoes.R
@@ -1,26 +1,38 @@
 # Funções -----------------------------------------------------------------
 
-# Relembrando, o que são funcões?
+# Relembrando: o que são funcões?
 # Funções são nomes que guardam um código de R. Esse código é
 # avaliado quando rodamos uma função.
 
-# As funções permitem automatizar algumas tarefas comuns de uma forma mais poderosa
-# e geral do que copiar e colar. 
+# As funções permitem automatizar algumas tarefas comuns de uma forma mais 
+# poderosa e geral do que copiar e colar. 
 
-# a função `c()` foi utilizada para criar vetores;
-# a função `class()` foi utilizada para descobrir a classe de um objeto;
-
-# Argumentos
+## Argumentos --------------------------
 
 # Argumentos são sempre separados por vírgulas
 
 c(1, 3, 5)
 
-# Alguns argumentos tem valores por padrão. 
+
+# 2 conjuntos mais amplos de argumentos:
+
+# 1) argumentos que oferecem os dados para a função
+
+# 2) argumentos que controlam os detalhes do que deve ser executado.
+
+# Geralmente os argumentos relativos aos dados são os primeiros. 
+
+
+
+# Alguns argumentos tem valores por padrão, que costumam ser os valores mais
+# comuns a serem utilizados.
+
 # EX. função round() é utilizada para arredondar valores.
+
 # Ela recebe os argumentos: 
 # x - o número para arredondar, e
 # digits - o número de casas decimais para serem mantidas.
+
 # Caso não seja informada quantas casas decimais devem ser mantidas,
 # utilizando o argumento digits, o valor padrão utilizado será 0, e o
 # número será arredondado para não ter nenhuma casa decimal.
@@ -57,11 +69,55 @@ help(round)
 # Construindo funções no R ------------------------
 
 
-# Conteudo
-# - construindo funções em R
-# - argumentos
-# - simplificando scripts usando funções
-# 
-# - Exemplo: simplificar o script final da primeira aula
-# 
-# - a função source 
+# Estrutura de uma função
+
+# nome_da_funcao <- function(argumentos){
+#   corpo_da_funcao
+# }
+
+# As funções devem ter nomes! 
+# Listar os argumentos da função que você quer criar dentro da funcão function() .
+# Depois dos argumentos, dentro das chaves {}, escrever o corpo da função,
+# ou seja, o que deve ser executado quando a função for utilizada. 
+
+# Dica: É mais fácil começar com um código que funciona e converte-lo em uma função.
+
+# Exemplo: Cálculo do IMC
+# Primeiro vamos fazer um código simples, e depois transformaremos em uma função.
+
+peso <- 65
+altura <- 1.75
+imc <- peso/ (altura^2)
+imc
+
+# Agora vamos transformar em uma função: primeiro, quais são os argumentos?
+# Ou seja, o que precisamos informar para que o cálculo seja realizado?
+# O peso e a altura! Portanto serão nossos argumentos.
+# E o que colocaremos no corpo da função? Justamente a fórmula para calcular o IMC.
+
+
+calculo_imc <- function(peso, altura){
+  imc <- peso/ (altura^2)
+  round(imc, 2)      # podemos usar a função  round para arredondar o resultado
+}
+
+# Agora que a função está criada, podemos testar com diferentes pesos e alturas!
+calculo_imc(peso = 65, altura = 1.75)
+
+
+# Função source ------------------------------------------------------
+
+# Para organizar  o projeto e o código, podemos deixar as funções em outro(s) 
+# arquivo(s) .R,  e carregá-las no script que estamos utilizando com a função source():
+
+# source("caminho_para_o_arquivo.R")
+
+# Atenção: a função source() vai executar todo o código que está no arquivo! 
+# Caso no arquivo tenha variáveis sendo criadas, elas serão carregadas também.
+
+# Ex:
+listar_arquivos("exemplos_de_aula/")
+
+source("exemplos_de_aula/exemplo-source.R")
+
+listar_arquivos("exemplos_de_aula/")
