88b4cb2ebbcad2923bdc56c584f20082e4e98712
Beatriz Milz <beatriz.milz@hotmail.com>
2021-01-30 09:33:02 -0300

    remove um exercício da aula 1 e coloca na aula 2

--- exercicios/1-introducao.R
+++ exercicios/1-introducao.R
@@ -14,16 +14,3 @@ c(1,2,3) - 1           # código 1
 c(1,2,3) - c(1,1,1)    # código 2
 # Os resultados são iguais? Porquê?
 
-# 2. Considere o vetor booleano abaixo:
-dolar_subiu <- c(TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE)
-
-# Este vetor tem informação de uma semana (7 dias) indicando se o dolar subiu (TRUE)
-# (ou não subiu - FALSE) no respectivo dia. Interprete os números abaixo:
-
-# (a) length(dolar_subiu)
-# (b) dolar_subiu[2]  (ps: a semana começa no domingo)
-# (c) sum(dolar_subiu)
-# (d) mean(dolar_subiu)
-
-# Agora observe a saída de as.numeric(dolar_subiu). O que o R fez?
-
