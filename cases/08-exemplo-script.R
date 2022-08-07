fb6c17cab9d2db35bef2c870a975f4d7d179637b
Beatriz Milz <milz.bea@gmail.com>
2022-08-07 10:14:14 -0300

    quebra linhas - arquivo nao comitado

--- cases/08-exemplo-script.R
+++ cases/08-exemplo-script.R
@@ -1,9 +1,11 @@
 # Esse é o nosso quarto exemplo de script
 
-# Ele é uma sequência de passos que começa com a criação de um vetor de dados e depois
+# Ele é uma sequência de passos que começa com a criação de um vetor de dados e
+# depois
 
 # 1. Calcula uma série de estatísticas desse vetor
-# 2. Imprime essas estatísticas em texto para que você possa colar isso em outro lugar
+# 2. Imprime essas estatísticas em texto para que você possa colar isso em outro
+# lugar
 # como um relatório, uma apresentação ou outro tipo de documento.
 
 
@@ -13,8 +15,10 @@ library(readr)
 
 # Lista de arquivos -------------------------------------------------------
 
-arquivos <- c("dados/voos_de_janeiro.csv", "dados/voos_de_fevereiro.csv", "dados/voos_de_marco.csv",
-              "dados/voos_de_abril.csv", "dados/voos_de_maio.csv", "dados/voos_de_junho.csv")
+arquivos <- c("dados/voos_de_janeiro.csv", "dados/voos_de_fevereiro.csv", 
+              "dados/voos_de_marco.csv",
+              "dados/voos_de_abril.csv", "dados/voos_de_maio.csv",
+              "dados/voos_de_junho.csv")
 
 
 # Parâmetros do script ----------------------------------------------------
@@ -28,7 +32,8 @@ for(arquivo in arquivos){
   dados <- read_csv2(arquivo,
                             col_types = "dddddddddcdcccddddT",
                             col_names = TRUE,
-                            locale = locale(decimal_mark = ",", grouping_mark = "."))
+                            locale = locale(decimal_mark = ",", 
+                                            grouping_mark = "."))
   
   # Filtrando os dados! ----------------------------------------------------
   
@@ -44,12 +49,14 @@ for(arquivo in arquivos){
   
   if(media > meta){
     
-    mensagem_final <- paste("No arquivo ", arquivo, " a média do atraso foi de", media,
+    mensagem_final <- paste("No arquivo ", arquivo, " a média do atraso foi de",
+                            media,
           "minutos, o que é maior do que a meta de", meta, "minutos.")
     
   } else if(media <= meta) {
     
-    mensagem_final <- paste("[ALERTA - NÃO BATEU A META] No arquivo ", arquivo, " a média do atraso foi de", media,
+    mensagem_final <- paste("[ALERTA - NÃO BATEU A META] No arquivo ",
+                            arquivo, " a média do atraso foi de", media,
           "minutos, o que é menor do que a meta de", meta, "minutos.")
     
   }
@@ -62,6 +69,7 @@ for(arquivo in arquivos){
 
 # 1. Amplie a abrangência desse script para os 12 meses de dados
 
-# 2. Adapte o script para que ele tome a média em todos os voos, não mais em só nos que tem origem JFK e destino ATL
+# 2. Adapte o script para que ele tome a média em todos os voos, 
+# não mais em só nos que tem origem JFK e destino ATL
 
 
